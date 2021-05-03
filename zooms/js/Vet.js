560cc2bcc31c3dd0241014fe593f74d6f017fd12
thilton203 <hiltont1@southernct.edu>
2021-05-02 23:54:29 -0400

    Update Vet.js

--- zooms/js/Vet.js
+++ zooms/js/Vet.js
@@ -1,16 +1,20 @@
 let showNewAnimalRecordForm = document.getElementById('newAnimalForm');
 let showUpdateAnimalRecordForm = document.getElementById('updateAnimalForm');
+let showAllNutritionRecords = document.getElementById('allNutritionRecords');
 
 
 window.onload = function() {
 	showNewAnimalRecordForm.style.display = 'none';
 	showUpdateAnimalRecordForm.style.display = 'none';
+	showAllNutritionRecords.style.display = 'none';
 }
 
 let addNewAnimalButton = document.getElementById('addNewAnimalRecordButton');
 addNewAnimalButton.addEventListener("click", function() {
 	showNewAnimalRecordForm.style.display = 'flex';
 	showUpdateAnimalRecordForm.style.display = 'none';
+	showAllNutritionRecords.style.display = 'none';
+
 	document.getElementById('submitNewAnimalRecord').name = "submitNewRecord";
 });
 
@@ -18,5 +22,14 @@ let updateAnimalDietButton = document.getElementById('updateDietButton');
 updateAnimalDietButton.addEventListener("click", function() {
 	showUpdateAnimalRecordForm.style.display = 'flex';
 	showNewAnimalRecordForm.style.display = 'none';
+	showAllNutritionRecords.style.display = 'none';
 	document.getElementById('submitUpdateAnimalRecord').name = "submitUpdateRecord";
+});
+
+let showAllNutritionButton = document.getElementById('viewAllNutritionRecordButton');
+showAllNutritionButton.addEventListener("click", function() {
+	showAllNutritionRecords.style.display = 'flex';
+	showNewAnimalRecordForm.style.display = 'none';
+	showUpdateAnimalRecordForm.style.display = 'none';
+	
 });
