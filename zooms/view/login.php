178b898b85b21229fdf27d2a76faee3afa1c9d09
bpatricka <arbachouskb1@southernct.edu>
2021-05-02 23:49:47 -0400

    added login, logout, pw reset for current user, weather for zoo

---
+++ zooms/view/login.php
@@ -0,0 +1,137 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+$myRoot = $_SERVER["DOCUMENT_ROOT"];
+
+// Include connection file
+require_once ($myRoot . '/myproject/db/connect_to_db.php');
+$conn = get_db_connection();
+
+// Define variables and initialize with empty values
+$employee_id = $password = "";
+$employee_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ /**************************
+  * 
+  * Form error Handling 
+  *
+  * start
+  *
+  *
+  *************************/
+    // Check if employee_id is empty
+    if(empty(trim($_POST["employee_id"]))){
+        $employee_err = "Please enter employee id.";
+    } else{
+        $employee_id = intval(trim($_POST["employee_id"]));
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    /******************************
+     * 
+     * end
+     * 
+     *******************************/
+    
+    // Validate credentials
+    if(empty($employee_err) && empty($password_err)){
+
+        // Prepare a select statement
+        $sql = "SELECT ID, password, fname FROM employees WHERE ID = ?";
+
+        if($result = sqlsrv_query($conn, $sql, array($employee_id))){
+            if(sqlsrv_has_rows($result)){ 
+                if($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
+                    
+                    // can change to password_verify($password, $row['password'])
+                    // to verify hashed passwords
+                    if($password == $row['password']){
+                        // Password is correct, so start a new session
+                        session_start();
+                        
+                        // Store data in session variables
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["employee_id"] = $employee_id;
+                        $_SESSION["employee_name"] = $row['fname'];
+
+                        // Redirect user to welcome page
+                        header("location: welcome.php");
+                    } else{
+                        // Password is not valid, display a generic error message
+                        $login_err = "Invalid employee_id or password.";
+                    }
+                }
+            } else{
+                // employee_id doesn't exist, display a generic error message
+                $login_err = "Invalid employee_id or password.";
+            }
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+            die( print_r( sqlsrv_errors(), true));
+        }
+
+        // Close statement
+        sqlsrv_free_stmt($result);
+    }
+    
+    // Close connection
+    sqlsrv_close($conn);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <!-- Forms for user input -->
+        <!-- html special chars prevent XSS hacking -->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>employee_id</label>
+                <input type="text" name="employee_id" class="form-control <?php echo (!empty($employee_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $employee_id; ?>">
+                <span class="invalid-feedback"><?php echo $employee_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ zooms/view/weather.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+
+        .report-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+    </style>
+    <title>Zoo Weather</title>
+</head>
+    <body>
+        <?php
+        $apiKey = "8319139fb9edf73b366bdadd9adbfbad";
+        $cityId = "5110253";
+        $weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+        
+        // curl session
+        $ch = curl_init();
+
+        // options for curl
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $weatherApiUrl);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // response object
+        $response = curl_exec($ch);
+
+        // close curl session
+        curl_close($ch);
+        // decode json
+        $data = json_decode($response);
+
+        // set our timezone and get current time
+        date_default_timezone_set('America/New_York');
+        $currentTime = time();
+
+		echo file_get_contents("../html/header.html");
+        ?>
+        <div id="w-banner">
+        </div>
+        <div class="report-container">
+            <h2 id='w-head'>Today's Weather at the Zoo</h2>
+            <h5><?php echo $data->name; ?> Weather Status</h2>
+            <div class="time">
+                <div><?php echo date("l g:i a", $currentTime); ?></div>
+                <div><?php echo date("jS F, Y",$currentTime); ?></div>
+                <div><?php echo ucwords($data->weather[0]->description); ?></div>
+            </div>
+            <div class="weather-forecast">
+                <img
+                    src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
+                    class="weather-icon" /> 
+                    <table>
+                        <tr>
+                            <th>
+                                High
+                            </th>
+                            <th>
+                                Low
+                            </th>
+                        </tr>
+                        <tr>
+                            <td id='max-temp'>
+                                <?php echo $data->main->temp_max; ?>°C
+                            </td>
+                            <td id='min-temp'>
+                                <?php echo $data->main->temp_min; ?>°C
+                            </td>
+                        </tr>
+                    </table>
+            </div>
+            <div class="weather-more">
+                <div>Humidity: <?php echo $data->main->humidity; ?> %</div>
+                <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+            </div>
+        </div>
+    </body>
+</html>
