5cc8e07521e3f408c9d7bb0eefe45eb6e8e1284a
thilton203 <hiltont1@southernct.edu>
2021-05-02 23:55:26 -0400

    Update javascript.js

--- zooms/js/javascript.js
+++ zooms/js/javascript.js
@@ -1,12 +1,23 @@
-let showForm = document.getElementById('form');
+let showNewForm = document.getElementById('form');
+let showUpdateForm = document.getElementById('updateForm');
 
 
 window.onload = function() {
-	showForm.style.display = 'none';
+	showNewForm.style.display = 'none';
+	showUpdateForm.style.display = 'none';
 
 }
 
 let addNewEmployeeButton = document.getElementById('addNewEmployeeButton');
 addNewEmployeeButton.addEventListener("click", function() {
-	showForm.style.display = 'flex';
+	showNewForm.style.display = 'flex';
+	showUpdateForm.style.display = 'none';
+	document.getElementById('submitNewEmployeeRecord').name = "submitNewEmployeeRecord";
+});
+
+let updateEmployeeButton = document.getElementById('updateEmployeeButton');
+updateEmployeeButton.addEventListener("click", function() {
+	showUpdateForm.style.display = 'flex';
+	showNewForm.style.display = 'none';
+	document.getElementById('submitUpdateEmployeeRecord').name = "submitUpdateEmployeeRecord";
 });
