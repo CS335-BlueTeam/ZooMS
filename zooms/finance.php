98fc1340d81edfd3994760ea2271f66260f0a9e2
thilton203 <hiltont1@southernct.edu>
2021-05-05 14:54:29 -0400

    added finance back

--- zooms/finance.php
+++ zooms/finance.php
@@ -6,12 +6,12 @@ session_start();
  
 // Check if the user is logged in, if not then redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: ./view/login.php");
+    header("location: login.php");
     echo "You are not logged in.";
     exit;
 }
     elseif($_SESSION["department"]!=='Accounting'){
-        header("location: ./view/login.php");
+        header("location: login.php");
         echo "You are not in Accounting.";
         exit;
     }
