ab197e131028506664478b45da027c0243d87fab
bpatricka <arbachouskb1@southernct.edu>
2021-05-05 13:27:32 -0400

    finance

--- zooms/html/header.php
+++ zooms/html/header.php
@@ -23,6 +23,7 @@
                                 <li class="nav-item"><a class="nav-link" href="#">Tickets</a></li>
                                 <li class="nav-item"><a class="nav-link" href="#">Logs</a></li>
                                 <li class="nav-item"><a class="nav-link" href="/zooms/zooms/view/weather.php">Weather</a></li>
+								<li class="nav-item"><a class="nav-link" href="/zooms/zooms/finance.php">Finance</a></li>
                                 <li class="nav-item"><a class="nav-link" href="/zooms/zooms/HR_admin_page.php">Human Resources</a></li>
                                 <li class="nav-item"><a class="nav-link" href="/zooms/zooms/Veterinarian_admin_page.php">Veterinarian Services</a></li>
 
