7bf38175159b471fbd71cecafa0e7c1f7f411ae1
bpatricka <arbachouskb1@southernct.edu>
2021-05-05 14:07:37 -0400

    fixed path

--- zooms/finance.php
+++ zooms/finance.php
@@ -6,12 +6,12 @@ session_start();
  
 // Check if the user is logged in, if not then redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    header("location: ./view/login.php");
     echo "You are not logged in.";
     exit;
 }
     elseif($_SESSION["department"]!=='Accounting'){
-        header("location: login.php");
+        header("location: ./view/login.php");
         echo "You are not in Accounting.";
         exit;
     }
